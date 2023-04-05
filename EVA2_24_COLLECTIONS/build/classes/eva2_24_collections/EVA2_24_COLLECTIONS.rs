54768fb20c505ee0aff6846491e95c8c8cb8b663
Yessly Mayorga <106281825+YessPuentes12@users.noreply.github.com>
2023-04-04 22:05:16 -0600

    Add files via upload

---
+++ EVA2_24_COLLECTIONS/build/classes/eva2_24_collections/EVA2_24_COLLECTIONS.rs
@@ -0,0 +1,2 @@
+eva2_24_collections.Persona
+eva2_24_collections.EVA2_24_COLLECTIONS
---
+++ EVA2_24_COLLECTIONS/src/eva2_24_collections/EVA2_24_COLLECTIONS.java
@@ -0,0 +1,30 @@
+/*
+
+ */
+package eva2_24_collections;
+
+import java.util.ArrayList;
+
+public class EVA2_24_COLLECTIONS {
+
+
+    public static void main(String[] args) {
+      ArrayList arreglo = new ArrayList();
+      
+      arreglo.add(new Persona());
+      
+      ArrayList <Integer> arreglo2 = new ArrayList <Integer>();
+      
+      arreglo2.add(150);
+      arreglo2.add(222);
+      arreglo2.add(294);
+      arreglo2.add(366);
+      
+       System.out.println(arreglo2);
+    }
+    
+}
+
+class Persona{
+    
+}
